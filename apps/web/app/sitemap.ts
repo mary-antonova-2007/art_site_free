f4c4cff96aea7556949c1a0a74bcd5370c507aef
Antonov Dmitriy <mailbox.antonov@gmail.com>
2026-05-19 13:38:25 +0300

    Make sitemap generation runtime dynamic

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -3,6 +3,8 @@ import type { MetadataRoute } from "next";
 import { getSeoSettings, listPublishedPagesForSeo } from "@/lib/content-service";
 import { getSiteUrl, normalizeCanonicalPath, pageShouldBeInSitemap } from "@/lib/seo";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const [pages, settings] = await Promise.all([listPublishedPagesForSeo(), getSeoSettings()]);
   const siteUrl = getSiteUrl();
